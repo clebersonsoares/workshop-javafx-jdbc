9c5d03e9689e8f0aede994585c2d53d34bff17cf
clebersonsoares <clebersonxadrez@hotmail.com>
2022-05-19 07:00:00 -0300

    Created MainViewController

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,8 +2,8 @@ package application;
 	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 
@@ -12,11 +12,14 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			Parent parent = loader.load();
+			ScrollPane scrollPane = loader.load();
 			
-			Scene mainScene = new Scene(parent);
+			Scene mainScene = new Scene(scrollPane);
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+			
 			primaryStage.setScene(mainScene);
-			primaryStage.setTitle("Sample Java Application")
+			primaryStage.setTitle("Sample Java Application");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
