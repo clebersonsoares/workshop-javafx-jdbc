6cfeb4f36ab8d34fff710fc52716463bf02ff03c
clebersonsoares <clebersonxadrez@hotmail.com>
2022-05-30 20:44:17 -0300

    Create Department Form

---
+++ src/gui/util/Utils.java
@@ -0,0 +1,12 @@
+package gui.util;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class Utils {
+	//Retorna o palco atual
+	public static Stage currentStage(ActionEvent event) {
+		return (Stage) ((Node) event.getSource()).getScene().getWindow();
+	}
+}
